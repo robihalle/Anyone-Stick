36d66d96a9125bd33c9557a05dbe6a04fe76dc58
robihalle <robertmoebes@googlemail.com>
2026-02-09 12:51:18 +0100

    Add files via upload
    new mode_normal

--- mode_normal.sh
+++ mode_normal.sh
@@ -9,4 +9,12 @@ iptables -A FORWARD -i usb0 -o wlan0 -j ACCEPT
 iptables -A FORWARD -i wlan0 -o usb0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
+# Exit-Country-Auswahl zurücksetzen
+sed -i '/^ExitNodes/d' /etc/anonrc
+sed -i '/^StrictNodes/d' /etc/anonrc
+
+# Anon-Config neu laden (falls Prozess läuft)
+pkill -SIGHUP -x anon 2>/dev/null || true
+
+# LED: dauerhaft an = Normal Mode
 echo default-on | sudo tee /sys/class/leds/default-on/trigger
